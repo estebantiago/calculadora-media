9a7d88c2aa1929decf773b5ca9eafcb5eb9a02ab
estebantiago <168283080+estebantiago@users.noreply.github.com>
2025-08-12 12:28:35 -0300

    primeiro commit

---
+++ src/script.js
@@ -0,0 +1,17 @@
+var distancia = document.getElementById('txtkm');
+var precoCombust = document.querySelector('input#txtpreco');
+var consmedio = document.querySelector('input#txtcons');
+var res = document.querySelector('div#res');
+
+function calcular(){
+
+   if(distancia.value.trim() === "" || precoCombust.value.trim() === "" || consmedio.value.trim() === ""){
+      alert('[ERRO] Você esqueceu de digitar ou algum dado está incorreto, tente novamente!')
+      return;
+   }
+   
+   var dis = Number(distancia.value) / Number(consmedio.value);
+   var resultado = dis * Number(precoCombust.value) ;
+
+   res.innerHTML=`Você vai gastar um total de R$ ${resultado.toFixed(2)}`;
+}
